e7e6b35a01df7ed6e6f80692057b822aef363310
Catherine <hofmanncaz@yandex.ru>
2025-07-18 04:28:33 +0300

    corrected locators NAME and PRICE

--- locators.py
+++ locators.py
@@ -1,5 +1,3 @@
-from playwright.sync_api import Page, expect
-
 class PageLocators:
 
     LOGIN = '#user-name'
@@ -12,19 +10,10 @@ class PageLocators:
 
     SORT = 'product-sort-container'
 
-    NAME1 = 'inventory-item-name'
-    NAME2 = 'inventory-item-name'
-    NAME3 = 'inventory-item-name'
-    NAME4 = 'inventory-item-name'
-    NAME5 = 'inventory-item-name'
-    NAME6 = 'inventory-item-name'
+    NAME = 'inventory-item-name'
 
-    PRICE1 = 'inventory-item-price'
-    PRICE2 = 'inventory-item-price'
-    PRICE3 = 'inventory-item-price'
-    PRICE4 = 'inventory-item-price'
-    PRICE5 = 'inventory-item-price'
-    PRICE6 = 'inventory-item-price'
+    PRICE = 'inventory-item-price'
+
 
 
 
--- test_shopping_cart_container.py
+++ test_shopping_cart_container.py
@@ -1,6 +1,5 @@
 import pytest
 from playwright.sync_api import Page, expect
-from playwright.sync_api import sync_playwright
 from .locators import PageLocators
 
 @pytest.fixture
@@ -37,46 +36,46 @@ def test_add_multiple_item_to_cart_and_remove(page: Page, authorization):
 def test_name_filter(page: Page, authorization):
     page.get_by_test_id(PageLocators.SORT).select_option('az')
 
-    name1 = page.get_by_test_id(PageLocators.NAME1).nth(0).text_content()
-    name2 = page.get_by_test_id(PageLocators.NAME2).nth(1).text_content()
-    name3 = page.get_by_test_id(PageLocators.NAME3).nth(2).text_content()
-    name4 = page.get_by_test_id(PageLocators.NAME4).nth(3).text_content()
-    name5 = page.get_by_test_id(PageLocators.NAME5).nth(4).text_content()
-    name6 = page.get_by_test_id(PageLocators.NAME6).nth(5).text_content()
+    name1 = page.get_by_test_id(PageLocators.NAME).nth(0).text_content()
+    name2 = page.get_by_test_id(PageLocators.NAME).nth(1).text_content()
+    name3 = page.get_by_test_id(PageLocators.NAME).nth(2).text_content()
+    name4 = page.get_by_test_id(PageLocators.NAME).nth(3).text_content()
+    name5 = page.get_by_test_id(PageLocators.NAME).nth(4).text_content()
+    name6 = page.get_by_test_id(PageLocators.NAME).nth(5).text_content()
 
     assert name1 < name2 < name3 < name4 < name5 < name6, 'Wrong filtration A-Z'
 
     page.get_by_test_id(PageLocators.SORT).select_option('za') #сортировка в обратном порядке
 
-    name1 = page.get_by_test_id(PageLocators.NAME1).nth(0).text_content()
-    name2 = page.get_by_test_id(PageLocators.NAME2).nth(1).text_content()
-    name3 = page.get_by_test_id(PageLocators.NAME3).nth(2).text_content()
-    name4 = page.get_by_test_id(PageLocators.NAME4).nth(3).text_content()
-    name5 = page.get_by_test_id(PageLocators.NAME5).nth(4).text_content()
-    name6 = page.get_by_test_id(PageLocators.NAME6).nth(5).text_content()
+    name1 = page.get_by_test_id(PageLocators.NAME).nth(0).text_content()
+    name2 = page.get_by_test_id(PageLocators.NAME).nth(1).text_content()
+    name3 = page.get_by_test_id(PageLocators.NAME).nth(2).text_content()
+    name4 = page.get_by_test_id(PageLocators.NAME).nth(3).text_content()
+    name5 = page.get_by_test_id(PageLocators.NAME).nth(4).text_content()
+    name6 = page.get_by_test_id(PageLocators.NAME).nth(5).text_content()
 
     assert name1 > name2 > name3 > name4 > name5 > name6, 'Wrong filtration Z-A'
 
 def test_price_filter(page: Page, authorization):
     page.get_by_test_id(PageLocators.SORT).select_option('lohi')
 
-    price1 = page.get_by_test_id(PageLocators.PRICE1).nth(0).text_content()
-    price2 = page.get_by_test_id(PageLocators.PRICE2).nth(1).text_content()
-    price3 = page.get_by_test_id(PageLocators.PRICE3).nth(2).text_content()
-    price4 = page.get_by_test_id(PageLocators.PRICE4).nth(3).text_content()
-    price5 = page.get_by_test_id(PageLocators.PRICE5).nth(4).text_content()
-    price6 = page.get_by_test_id(PageLocators.PRICE6).nth(5).text_content()
+    price1 = page.get_by_test_id(PageLocators.PRICE).nth(0).text_content()
+    price2 = page.get_by_test_id(PageLocators.PRICE).nth(1).text_content()
+    price3 = page.get_by_test_id(PageLocators.PRICE).nth(2).text_content()
+    price4 = page.get_by_test_id(PageLocators.PRICE).nth(3).text_content()
+    price5 = page.get_by_test_id(PageLocators.PRICE).nth(4).text_content()
+    price6 = page.get_by_test_id(PageLocators.PRICE).nth(5).text_content()
 
     assert float(price1[1:]) <= float(price2[1:]) <= float(price3[1:]) <= float(price4[1:]) <= float(price5[1:]) <= float(price6[1:]), 'Wrong filtration ASC'
 
     page.get_by_test_id(PageLocators.SORT).select_option('hilo') #сортировка в обратном порядке
 
-    price1 = page.get_by_test_id(PageLocators.PRICE1).nth(0).text_content()
-    price2 = page.get_by_test_id(PageLocators.PRICE2).nth(1).text_content()
-    price3 = page.get_by_test_id(PageLocators.PRICE3).nth(2).text_content()
-    price4 = page.get_by_test_id(PageLocators.PRICE4).nth(3).text_content()
-    price5 = page.get_by_test_id(PageLocators.PRICE5).nth(4).text_content()
-    price6 = page.get_by_test_id(PageLocators.PRICE6).nth(5).text_content()
+    price1 = page.get_by_test_id(PageLocators.PRICE).nth(0).text_content()
+    price2 = page.get_by_test_id(PageLocators.PRICE).nth(1).text_content()
+    price3 = page.get_by_test_id(PageLocators.PRICE).nth(2).text_content()
+    price4 = page.get_by_test_id(PageLocators.PRICE).nth(3).text_content()
+    price5 = page.get_by_test_id(PageLocators.PRICE).nth(4).text_content()
+    price6 = page.get_by_test_id(PageLocators.PRICE).nth(5).text_content()
 
     assert float(price1[1:]) >= float(price2[1:]) >= float(price3[1:]) >= float(price4[1:]) >= float(price5[1:]) >= float(price6[1:]), 'Wrong filtration DESC'
 
