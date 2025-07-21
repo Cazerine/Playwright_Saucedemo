a6432af428e5c69dd4f36ca285e50c91f97572f9
Catherine <hofmanncaz@yandex.ru>
2025-07-21 07:01:49 +0300

    updated locators.py

--- pages/locators.py
+++ pages/locators.py
@@ -15,12 +15,21 @@ class PageLocators:
     ITEM = 'inventory-item-description'
     REM_ITEM1 = '#remove-sauce-labs-backpack'
     CART_BADGE = 'shopping-cart-badge'
+    ITEM_BACKPACK = "#item_4_title_link"
+    ALL_ITEMS = "#inventory_sidebar_link"
+    ABOUT = "#about_sidebar_link"
+    LOGOUT = "#logout_sidebar_link"
+    ROOT = "#root"
+    ITEM_CART = "#inventory_item_container"
+    BACK = "#back-to-products"
 
     SORT = 'product-sort-container'
 
     NAME = 'inventory-item-name'
 
     PRICE = 'inventory-item-price'
+
+    BURGER_MENU = "#react-burger-menu-btn" #ID
 
 
 
